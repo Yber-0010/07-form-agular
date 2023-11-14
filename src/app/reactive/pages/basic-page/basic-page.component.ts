28aef1d122241a47ee43e53586da4504028fd9c3
Iber Chambi <ichambi@datec.com.bo>
2023-11-14 16:00:06 -0400

    validaciones

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5050 = {
+  name: "rtx 5050",
+  price: 2500,
+  inStorage: 6
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+
+
+
+export class BasicPageComponent implements OnInit {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['',[Validators.required,Validators.minLength(3)],[]],
+    price: [0,[Validators.required,Validators.min(0)],[]],
+    inStorage: [0,[Validators.required,Validators.min(0)],[]],
+  })
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    // this.myForm.reset(rtx5050)
+  }
+
+  onSave():void {
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched
+      return
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset({price: 0, inStorage: 0})
+  }
 
 }
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { ReactiveRoutingModule } from './reactive-routing.module';
+
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { DinamicPageComponent } from './pages/dinamic-page/dinamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
@@ -9,7 +10,8 @@ import { SwitchesPageComponent } from './pages/switches-page/switches-page.compo
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [
     BasicPageComponent,
